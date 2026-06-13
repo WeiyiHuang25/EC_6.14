563840f910519832fdafde8be49f549586baa496
WeiyiHuang25 <weiyihuang@link.cuhk.edu.cn>
2026-06-13 17:31:15 +0800

    完成encoder驱动

--- Projects/Project/User/bsp_encoder.h
+++ Projects/Project/User/bsp_encoder.h
@@ -8,7 +8,4 @@
 #define ENCODER_GEAR_RATIO          21.3f
 
 void Encoder_Init(void);
-int32_t Encoder_GetCount(void);
-void Encoder_Reset(void);
-float Encoder_GetMotorRPM(void);
 float Encoder_GetRPM(void);
