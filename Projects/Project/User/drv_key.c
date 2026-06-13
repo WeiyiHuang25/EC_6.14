cb3bcea1789c2a93d5cbaac6335b179936b26b24
WeiyiHuang25 <125090226@link.cuhk.edu.cn>
2026-06-13 20:19:19 +0800

    fix key

--- Projects/Project/User/drv_key.c
+++ Projects/Project/User/drv_key.c
@@ -4,7 +4,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
     switch(GPIO_Pin)
     {
-        case LED_R_Pin:
+        case KEY_Pin:
         {
             HAL_GPIO_TogglePin(LED_R_GPIO_Port, LED_R_Pin);
         }break;
